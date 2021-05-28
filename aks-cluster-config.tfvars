a924f7a3dc074a5186a9ba670ecc7c30a379f909
Mirah Gary <mgary@vmware.com>
2021-05-28 14:18:54 +0200

    Add support for AKS.

---
+++ aks-cluster-config.tfvars
@@ -0,0 +1,3 @@
+vm_size = "Standard_D32d_v4"
+node_count = 3
+disk_size_gb = 100
--- terraform/aks/aks-cluster.tf
+++ terraform/aks/aks-cluster.tf
@@ -1,14 +1,28 @@
-resource "azurerm_kubernetes_cluster" "default" {
+resource "azurerm_kubernetes_cluster" "benchmark" {
   name                = "${random_pet.prefix.id}-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.benchmark.location
+  resource_group_name = azurerm_resource_group.benchmark.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
 
   default_node_pool {
-    name            = "default"
+    name            = "data-pool"
     node_count      = 3
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 100
+  }
+
+  resource "azurerm_kubernetes_cluster_node_pool" "rabbit-pool" {
+    name = "rabbit-pool"
+    kubernetes_cluster_id = azurerm_kubernetes_cluster.benchmark.id
+    vm_size = var.vm_size
+    node_count = var.node_count
+    os_disk_size_gb = var.disk_size_gb
+    node_labels = { rabbit-pool = true }
+    node_taints = [ "rabbit-pool=true:NoSchedule" ]
+
+    tags = {
+      Environment = "rabbit-pool"
+    }
   }
 
   service_principal {
